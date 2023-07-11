306e5cd6a10465a6ba08c4d705fb3ef796ee6a3c
pedro_reichow <pedroreichow3@gmail.com>
2023-07-11 10:43:12 -0300

    remove nextconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/{pedro-reichow-portfolio-2023}' : '',
-
-}
+const nextConfig = {}
 
 module.exports = nextConfig
