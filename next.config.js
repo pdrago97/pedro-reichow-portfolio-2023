75ca9958e3e92c824e85f7bb17ea641532adfe56
pedro_reichow <pedroreichow3@gmail.com>
2023-07-11 09:42:38 -0300

    add nextconfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/{pedro-reichow-portfolio-2023}' : '',
+
+}
 
 module.exports = nextConfig
